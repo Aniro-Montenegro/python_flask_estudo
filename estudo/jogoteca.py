67038dde0dc10400702ae878e06a5a7772ef9d55
Aniro <aniro.montenegro@flapper.aero>
2023-04-08 10:54:56 -0300

    formulario, renderizando, redirecionando pagina

--- estudo/jogoteca.py
+++ estudo/jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request,redirect
 
 
 class Jogo:
@@ -10,14 +10,30 @@ class Jogo:
 
 app = Flask(__name__)
 
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('Resident Evil', 'RPG', 'Playstation')
+jogo3 = Jogo('Final Fight', 'RPG', 'Nintendo')
+listaJogos = [jogo1, jogo2, jogo3]
 
-@app.route('/inicio')
-def ola():
-    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
-    jogo2 = Jogo('Resident Evil', 'RPG', 'Playstation')
-    jogo3 = Jogo('Final Fight', 'RPG', 'Nintendo')
-    listaJogos = [jogo1, jogo2, jogo3]
+
+@app.route('/')
+def index():
     return render_template('lista.html', titulo='Jogos', jogos=listaJogos)
 
 
-app.run()
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+
+@app.route('/criar', methods=['POST', ])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    listaJogos.append(jogo)
+    return redirect('/')
+
+
+app.run(debug=True)
